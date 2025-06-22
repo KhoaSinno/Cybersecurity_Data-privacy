11d40f0c32fff056a5ac4f03fb70d0201923469b
Sinoo <khoasinno@gmail.com>
2025-06-22 23:00:49 +0700

    fix deploy

--- setup.sh
+++ setup.sh
@@ -6,8 +6,14 @@ email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+[theme]\n\
+primaryColor = \"#1f77b4\"\n\
+backgroundColor = \"#ffffff\"\n\
+secondaryBackgroundColor = \"#f0f2f6\"\n\
+textColor = \"#262730\"\n\
+\n\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
+enableCORS = false\n\
 port = \$PORT\n\
 " > ~/.streamlit/config.toml
